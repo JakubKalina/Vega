797b36df2c7b7ba52b09575256c5dce033a95915
Jakub Kalina <jakub.kalina@codeteam.pl>
2019-08-16 00:21:14 +0200

    Added rendering checkboxes dynamically

--- ClientApp/src/app/vehicle-form/vehicle-form.component.ts
+++ ClientApp/src/app/vehicle-form/vehicle-form.component.ts
@@ -1,3 +1,4 @@
+import { FeatureService } from './../services/feature.service';
 import { Component, OnInit } from '@angular/core';
 import { MakeService } from '../services/make.service';
 
@@ -9,20 +10,23 @@ import { MakeService } from '../services/make.service';
 export class VehicleFormComponent implements OnInit {
   makes: any[];
   models: any[];
+  features: any[];
   vehicle: any = {};
 
-  constructor(private makeService: MakeService) { }
+  constructor(private makeService: MakeService, private featureService: FeatureService) { }
 
   ngOnInit() {
     this.makeService.getMakes().subscribe(makes =>
       this.makes = makes);
+
+    this.featureService.getFeatures().subscribe(features =>
+      this.features = features);
   }
 
   onMakeChange(makeId) {
     // tslint:disable-next-line:prefer-const
     let selectedMake = this.makes.find(m => m.id.toString() === makeId);
     this.models = selectedMake ? selectedMake.models : [];
-    console.log('Wybrane', selectedMake);
   }
 
 }
