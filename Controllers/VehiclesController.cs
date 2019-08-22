58437d2f4bacd8595d6612b9782f3b5ae6d0540c
Jakub Kalina <jakub.kalina@codeteam.pl>
2019-08-22 17:18:16 +0200

    Added possibility to create a new vehicle and add it to database

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -1,7 +1,9 @@
+using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using vega.Controllers.Resources;
 using vega.Models;
+using vega.Persistence;
 
 namespace vega.Controllers
 {
@@ -10,18 +12,25 @@ namespace vega.Controllers
     public class VehiclesController : Controller
     {
         private readonly IMapper mapper;
+        private readonly VegaDbContext context;
 
-        public VehiclesController(IMapper mapper)
+        public VehiclesController(IMapper mapper, VegaDbContext context)
         {
             this.mapper = mapper;
+            this.context = context;
         }
 
         // Temporary passing domain class !
         [HttpPost]
-        public IActionResult CreateVehicle([FromBody]VehicleResource vehicleResource) 
+        public async Task<IActionResult> CreateVehicle([FromBody]VehicleResource vehicleResource) 
         {
             var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
-            return Ok(vehicle);
+
+            context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(result);
         }
     }
 }
